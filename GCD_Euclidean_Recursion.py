bac4e0369b3dbc348e1588472db66c6b4a093569
Tiffany Schiff <ths205@alum.lehigh.edu>
2022-06-02 21:06:38 -0400

    Create GCD_Euclidean_Recursion.py

---
+++ GCD_Euclidean_Recursion.py
@@ -0,0 +1,36 @@
+
+#This program is to find the gcd using the Euclidean Algorithm and recursion
+
+def gcd(x,y):
+
+    if type(x) != int:
+       raise TypeError('You did not enter an integer for value x')
+
+    if type(y) != int:
+       raise TypeError('You did not enter an integer for value Y')
+
+    if  x < 0:
+
+       raise ValueError('You entered a negative number please enter zero or a positive number for x')
+
+    if  y < 0:
+
+       raise ValueError('You entered a negative number please enter zero or a positive number for y')
+
+
+    if x==0 and y >= 0:
+       return y
+
+    if x>=0 and y==0:
+       return x
+
+
+    quotient = x / y
+
+    remainder = x % y
+
+    return gcd(y, remainder)
+
+
+
+print(gcd(270,192))
