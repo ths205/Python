0f19a8673022950291e3256a8f120bd747cab4eb
Tiffany Schiff <ths205@alum.lehigh.edu>
2023-09-10 14:24:58 -0400

    Create sub_factorial.py
    This is the program used to calculate sub  factorial of a positive integer

---
+++ sub_factorial.py
@@ -0,0 +1,25 @@
+#This program is to calculate the subfactorial using the formula
+# !n = n * !(n-1)  + (-1)^(n)
+
+def factorial_sub(n):
+    if type(n)  !=  int:
+       raise TypeError('You did not enter an integer')
+
+    if n < 1:
+       raise ValueError('You entered  either  0  or a  negative number')
+
+    if n ==1:
+       return 0
+
+    if n==2:
+       return 1
+
+
+
+    return n * factorial_sub(n-1) + pow(-1,n)
+
+
+#print(factorial_sub(6))
+#print(factorial_sub(4))
+y=int(input('Please enter a positive integer'))
+print(factorial_sub(y))
