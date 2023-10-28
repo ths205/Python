f4e2017659d75eda2d1dc3a02a386f03288124fa
Tiffany Schiff <ths205@alum.lehigh.edu>
2023-10-27 21:41:37 -0400

    Create test_perfect_square.py

---
+++ test_perfect_square.py
@@ -0,0 +1,35 @@
+import perfect_square
+
+import unittest
+
+class Test_Perfect_Square(unittest.TestCase):
+
+    def test_raise_value_error(self):
+        #tests to  make sure  that the program does not  accept negative integers
+        self.assertRaises(ValueError,perfect_square.perfect_square,-1)
+
+    def test_raise_type_error_ch(self):
+        #tests to make sure that the program doesnot  accept  character values
+        self.assertRaises(TypeError, perfect_square.perfect_square,'c')
+
+    def test_raise_type_error_st(self):
+        #tests to make sure that the program doesnot  accept  string values
+        self.assertRaises(TypeError, perfect_square.perfect_square,"string#")
+
+    def test_raise_type_error_doub(self):
+        #tests to make sure that the program doesnot  accept  double values
+        self.assertRaises(TypeError, perfect_square.perfect_square,3.14)
+
+
+    def test_raise_type_error_flt(self):
+        #tests to make sure that the program doesnot  accept  double values
+        self.assertRaises(TypeError, perfect_square.perfect_square,3.14159265)
+
+    def test_assert_not_perfect_square(self):
+        self.assertEqual("It is not a perfect square",perfect_square.perfect_square(3))
+
+    def test_assert_perfect_square_equal(self):
+        self.assertEqual("It is a perfect square",perfect_square.perfect_square(1))
+
+if __name__== '__main__':
+   unittest.main()
