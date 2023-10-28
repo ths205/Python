115f3d2e7240887998366c2199b6b049f40bb55b
Tiffany Schiff <ths205@alum.lehigh.edu>
2023-10-27 21:42:49 -0400

    Create perfect_square.py

---
+++ perfect_square.py
@@ -0,0 +1,32 @@
+
+#This program returns whether an integer is a perfect square or not.
+
+def perfect_square(x):
+
+
+     if type(x) != int:
+
+        raise TypeError('Please  enter a  positive integer')
+
+     if x < 0:
+        raise ValueError('Please enter a number greater than 0')
+
+     for i in range(1, x+1):
+
+         if x/i == i:
+            return "It is a perfect square"
+
+         else:
+
+            continue
+
+     return "It is not a perfect square"
+
+
+#print(perfect_square(100))
+
+number =  int(input("Please Enter a Postive number"))
+
+
+
+print(perfect_square(number))
